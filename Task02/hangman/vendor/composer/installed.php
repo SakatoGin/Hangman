8f9b7c1ebb6578b4b33f5d1a9c82eec71a2efb9d
SakatoGin <vachumak0v@yandex.ru>
2025-09-16 20:52:35 +0300

    main

---
+++ Task02/hangman/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tanner/hangman',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8a973aa8a8800c490d738dd5cf876c10f9d0e20c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tanner/hangman' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8a973aa8a8800c490d738dd5cf876c10f9d0e20c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.6',
+            'version' => '0.12.6.0',
+            'reference' => 'f12b650d3738e471baed6dd47982d53c5c0ab1c3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
